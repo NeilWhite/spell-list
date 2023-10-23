a03d0a1689b162408127949af442769daa1086dc
Neil White <white.neil@gmail.com>
2023-10-23 03:08:04 -0700

    Hacky (but functional) spell advancement

--- scripts/module.js
+++ scripts/module.js
@@ -1,74 +1,20 @@
-const addonId = "spell-list";
+import { CONFIG, MODULE_NAME, preLoc, L } from "./config.js";
+import { Api } from "./api.js";
+import { renderItemSheetHook } from "./item-form.js";
+import { registerSettings } from "./settings.js";
+import { configureAdvancement } from "./spell-advancement.js";
 
-const mapDict = (type) => (acc, c) => {
-  acc[c] = { label: `SPELL-LIST.${type}.${c}` }
-  return acc;
-}
+Hooks.on("init", () => {
+  globalThis.CONFIG.SpellList = Api;
 
-const classes = [
-  "artificer", "bard", "cleric", "druid", "paladin",
-  "ranger", "sorcerer", "warlock", "wizard"
-];
-
-const subclasses = [
-  "oath-of-the-watchers"
-];
-
-const CONFIG = {
-  casters: {
-    class: classes.reduce(mapDict("class"), {}),
-    subclass: subclasses.reduce(mapDict("subclass"), {})
-  }
-};
-
-const L = (text) => game.i18n.localize(text);
-
-const preLoc = (dict, path) => {
-  for (const name in dict) dict[name][path] = L(dict[name][path])
-};
+  registerSettings();
+});
 
 Hooks.on("ready", () => {
   preLoc(CONFIG.casters.class, "label");
   preLoc(CONFIG.casters.subclass, "label");
-});
 
+  Hooks.on("renderItemSheet", renderItemSheetHook);
 
-const buildCheckbox = (name, detail, data) => {
-  return `<label class="checkbox">
-    <input type="checkbox" name="flags.${addonId}.${name}" ${data.flags[addonId][name] ? "checked" : ""} />
-    ${ detail.label }
-  </label>`;
-}
-
-Hooks.on("renderItemSheet", (itemSheet, html, { data }) => {
-
-  if (data.type !== "spell") return;
-
-  const element = html.find('select[name="system.preparation.mode"]').parent().parent();
-
-  if (element){
-    if (!data.flags[addonId]) foundry.utils.setProperty(data, `flags.${addonId}`, {});
-
-    const casterHtml = `<div>
-      <div class="form-group stacked">
-        <label>${L("SPELL-LIST.section.class")}</label>
-        ${Object
-          .entries(CONFIG.casters.class)
-          .map(([name, detail]) => buildCheckbox(name, detail, data) )
-          .join("")
-        }
-        <label>${L("SPELL-LIST.section.subclass")}</label>
-        ${Object
-          .entries(CONFIG.casters.subclass)
-          .map(([name, detail]) => buildCheckbox(name, detail, data) )
-          .join("")
-        }
-      </div>
-    </div>`;
-
-    element.after(
-      `<h3 class="form-header">${L("SPELL-LIST.name")}</h3>`,
-      casterHtml
-    );
-  }
+  configureAdvancement(game.system);
 });
