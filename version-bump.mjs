c5d3af513ce9dffa22ec67099710de4e39b098c3
Neil White <white.neil@gmail.com>
2023-10-26 00:47:54 -0700

    Thinking about publishing

--- version-bump.mjs
+++ version-bump.mjs
@@ -9,4 +9,7 @@ const updateFile = (fileName, update) => {
   writeFileSync(fileName, JSON.stringify(file, null, 2));
 };
 
-updateFile("module.json", u => u.version = targetVersion);
+updateFile("module.json", u => {
+  u.version = targetVersion;
+  u.download = `https://github.com/neilwhite/spell-list/releases/download/v${targetVersion}/spell-list.zip`;
+});
